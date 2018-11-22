640263ca8331b0c982862d027e77cfb7685ab6d3
Lewis Brooke <lewisbrooke@btinternet.com>
2018-11-22 22:50:25 +0000

    Simple change made to demo branch feature

--- Testing Repositories/ViewController.swift
+++ Testing Repositories/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("some change")
+        print("some changes made under Lewis' branch2")
     }
 
     override var representedObject: Any? {
