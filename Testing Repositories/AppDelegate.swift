7f8a1f253635e2d23f28c6ca4b6ea61c83fa7c77
Lewis Brooke <lewisbrooke@btinternet.com>
2018-11-22 22:31:08 +0000

    Initial Commit

---
+++ Testing Repositories/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Testing Repositories
+//
+//  Created by Lewis Brooke on 22/11/2018.
+//  Copyright © 2018 Lewis Brooke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Testing Repositories/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Testing Repositories
+//
+//  Created by Lewis Brooke on 22/11/2018.
+//  Copyright © 2018 Lewis Brooke. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
